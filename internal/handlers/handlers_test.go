eb709d23928c26efbb9b8db20f325d89c583261e
Manuel Pepe <manuelpepe-dev@outlook.com.ar>
2023-01-31 17:34:05 -0300

    improved TestIPHandler

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/manuelpepe/ip2p-server/internal/db"
+	"github.com/manuelpepe/ip2p-server/internal/ipconv"
 )
 
 var ExpectedISPInfo = []db.ISPInfo{
@@ -87,25 +88,39 @@ func TestISPHandler(t *testing.T) {
 }
 
 func TestIPHandler(t *testing.T) {
-	req := httptest.NewRequest(http.MethodGet, "/ip/23.227.38.83", nil)
-	// Required to manually set Vars as Router is not used (gorilla/mux/issues/373)
-	req = mux.SetURLVars(req, map[string]string{
-		"ip": "23.227.38.83",
-	})
-	w := httptest.NewRecorder()
-	server := Server{DB: &MockDB{}}
-	server.HandleIP(w, req)
-	res := w.Result()
-	defer res.Body.Close()
-	data, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		t.Errorf("expected error to be nil got %v", err)
+	type TC struct {
+		arg string
+		exp any // expected value will be marshaled
 	}
-	dataExpected, err := json.Marshal(ExpectedIPInfo)
-	if err != nil {
-		panic(err)
+
+	var tests = []TC{
+		{"23.227.38.83", ExpectedIPInfo},
+		{"23.1", ipconv.WrongIPFormatError.Error()},
+		{"23.227.38.83.123", ipconv.WrongIPFormatError.Error()},
+		{"255.255.255.256", ipconv.WrongIPFormatError.Error()},
 	}
-	if string(data) != string(dataExpected) {
-		t.Errorf("expected %s got %s", dataExpected, data)
+
+	for _, test := range tests {
+		req := httptest.NewRequest(http.MethodGet, "/ip/"+test.arg, nil)
+		// Required to manually set Vars as Router is not used (gorilla/mux/issues/373)
+		req = mux.SetURLVars(req, map[string]string{
+			"ip": test.arg,
+		})
+		w := httptest.NewRecorder()
+		server := Server{DB: &MockDB{}}
+		server.HandleIP(w, req)
+		res := w.Result()
+		defer res.Body.Close()
+		data, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			t.Errorf("expected error to be nil got %v", err)
+		}
+		dataExpected, err := json.Marshal(test.exp)
+		if err != nil {
+			panic(err)
+		}
+		if string(data) != string(dataExpected) {
+			t.Errorf("expected %s got %s", dataExpected, data)
+		}
 	}
 }
